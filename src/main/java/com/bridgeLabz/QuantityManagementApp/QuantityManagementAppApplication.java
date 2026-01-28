7eff03909f5ca618ffeab88e50b56655562755d9
sagarrj <sgrjaisinghani@gmail.com>
2026-01-28 23:13:17 +0530

    feet equality happy flow

--- src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
+++ src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
@@ -1,11 +1,47 @@
 package com.bridgeLabz.QuantityManagementApp;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class QuantityManagementAppApplication {
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter first value:");
+		String value1 = br.readLine();
+		System.out.println("enter second value:");
+		String value2 = br.readLine();
+		Feet feet1 = new Feet(Double.parseDouble(value1));
+		Feet feet2 = new Feet(Double.parseDouble(value2));
+		System.out.println(feet1.equals(feet2));
+	}
+
+	public static class Feet {
+		private final double value;
+
+		public Feet(double value) {
+			this.value = value;
+		}
+		public double getValue() {
+			return value;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || getClass() != o.getClass()) return false;
+			Feet feet = (Feet) o;
+			return Double.compare(value, feet.value) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(value);
+		}
+
+
 	}
 
 }
