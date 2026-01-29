76290ca280e4157235fdb9ef47c51e5e37699086
sagarrj <sgrjaisinghani@gmail.com>
2026-01-29 23:10:20 +0530

    added inch equality and testcases

--- src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
+++ src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
@@ -9,9 +9,15 @@ import java.util.Objects;
 public class QuantityManagementAppApplication {
 
 	public static void main(String[] args) throws IOException {
+
+		checkFeetEquality();
+		checkInchEquality();
+	}
+
+	private static void checkFeetEquality() throws IOException {
 		Feet feet1, feet2;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("enter first value:");
+		System.out.println("enter first ft. value:");
 		String value1 = br.readLine();
 		try{
 			feet1 = new Feet(Double.parseDouble(value1));
@@ -20,7 +26,7 @@ public class QuantityManagementAppApplication {
 			return;
 		}
 
-		System.out.println("enter second value:");
+		System.out.println("enter second ft. value:");
 		String value2 = br.readLine();
 		try{
 			feet2 = new Feet(Double.parseDouble(value2));
@@ -30,6 +36,30 @@ public class QuantityManagementAppApplication {
 		}
 
 		System.out.println(feet1.equals(feet2));
+	}
+
+	private static void checkInchEquality() throws IOException {
+		Inch inch1, inch2;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter first inch value:");
+		String value1 = br.readLine();
+		try{
+			inch1 = new Inch(Double.parseDouble(value1));
+		}catch (NumberFormatException e){
+			System.out.println("Invalid input. Please enter numeric values.");
+			return;
+		}
+
+		System.out.println("enter second inchvalue:");
+		String value2 = br.readLine();
+		try{
+			inch2 = new Inch(Double.parseDouble(value2));
+		}catch (NumberFormatException e){
+			System.out.println("Invalid input. Please enter numeric values.");
+			return;
+		}
+
+		System.out.println(inch1.equals(inch2));
 	}
 
 	public static class Feet {
@@ -56,5 +86,31 @@ public class QuantityManagementAppApplication {
 
 
 	}
+
+	public static class Inch {
+		private final double value;
+
+		public Inch(double value) {
+			this.value = value;
+		}
+		public double getValue() {
+			return value;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == null || getClass() != o.getClass()) return false;
+			Inch inch = (Inch) o;
+			return Double.compare(value, inch.value) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(value);
+		}
+
+
+	}
+
 
 }
