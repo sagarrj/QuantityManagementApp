bc1ec94c90a9a3bc8b457e10150f4a2de965e596
sagarrj <sgrjaisinghani@gmail.com>
2026-02-11 17:33:09 +0530

    testing Weight Quantity

--- src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
+++ src/main/java/com/bridgeLabz/QuantityManagementApp/QuantityManagementAppApplication.java
@@ -1,8 +1,8 @@
 package com.bridgeLabz.QuantityManagementApp;
 
 
-import com.bridgeLabz.QuantityManagementApp.length.Length;
-import com.bridgeLabz.QuantityManagementApp.length.LengthUnit;
+import com.bridgeLabz.QuantityManagementApp.weight.Weight;
+import com.bridgeLabz.QuantityManagementApp.weight.WeightUnit;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,26 +12,26 @@ import java.util.Arrays;
 public class QuantityManagementAppApplication {
 
 	public static void main(String[] args) throws IOException {
-		convertLength();
-
+        weightConversions();
 	}
 
-	private static void convertLength() throws IOException {
-        Length q1,q2;
+
+    private static void weightConversions() throws IOException {
+        Weight w1,w2;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         //first quantity
         System.out.println("enter first value:");
         String strValue1 = br.readLine();
 
-        System.out.println("enter first unit:" + Arrays.toString(LengthUnit.values()));
+        System.out.println("enter first unit:" + Arrays.toString(WeightUnit.values()));
         String unit1 = br.readLine();
-        LengthUnit lengthUnit1;
+        WeightUnit weightUnit1;
         double value1;
         try {
-            lengthUnit1 = LengthUnit.valueOf(unit1.toUpperCase());
+            weightUnit1 = WeightUnit.valueOf(unit1.toUpperCase());
             value1 = Double.parseDouble(strValue1);
-            q1 = new Length(value1, lengthUnit1);
+            w1 = new Weight(value1, weightUnit1);
         } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter finite numeric values. " + e.getMessage());
             return;
@@ -41,35 +41,35 @@ public class QuantityManagementAppApplication {
         System.out.println("enter second value:");
         String strValue2 = br.readLine();
 
-        System.out.println("enter second unit:" + Arrays.toString(LengthUnit.values()));
+        System.out.println("enter second unit:" + Arrays.toString(WeightUnit.values()));
         String unit2 = br.readLine();
-        LengthUnit lengthUnit2;
+        WeightUnit weightUnit2;
         double value2;
         try {
-            lengthUnit2 = LengthUnit.valueOf(unit2.toUpperCase());
+            weightUnit2 = WeightUnit.valueOf(unit2.toUpperCase());
             value2 = Double.parseDouble(strValue2);
-            q2 = new Length(value2, lengthUnit2);
+            w2 = new Weight(value2, weightUnit2);
         } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter finite numeric values. " + e.getMessage());
             return;
         }
 
-        Length addedInSource = q1.add(q2);
+        Weight addedInSource = w1.add(w2);
         System.out.println("Total of sum In Source:  " + addedInSource.getValue() + " " + addedInSource.getUnit().name());
 
 
-        System.out.println("enter target unit:" + Arrays.toString(LengthUnit.values()));
+        System.out.println("enter target unit:" + Arrays.toString(WeightUnit.values()));
         String unit3 = br.readLine();
-        LengthUnit lengthUnit3;
+        WeightUnit weightUnit3;
         try {
-            lengthUnit3 = LengthUnit.valueOf(unit3.toUpperCase());
+            weightUnit3 = WeightUnit.valueOf(unit3.toUpperCase());
         } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter numeric values.");
             return;
         }
 
-        Length totalInTarget = q1.add(q2, lengthUnit3);
-        System.out.println("Sum ( in  " +   lengthUnit3.name()  + ") :" + totalInTarget.getValue());
+        Weight totalInTarget = w1.add(w2, weightUnit3);
+        System.out.println("Sum ( in  " +   weightUnit3.name()  + ") :" + totalInTarget.getValue());
 
 
     }
